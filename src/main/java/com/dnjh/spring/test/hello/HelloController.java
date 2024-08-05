7e0df4abb73658f038b96472a9adc6da22263966
megait <megait@gmail.com>
2024-08-05 21:47:25 +0900

    [우재현]  - Spring Test 최초 파일 업로드

---
+++ src/main/java/com/dnjh/spring/test/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.dnjh.spring.test.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String hello()
+	{
+		return "Hellow World!!";
+	}
+}
