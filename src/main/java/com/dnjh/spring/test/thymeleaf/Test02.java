b85da2d0bb5f5a59b89f4196b0d4dae6712a7bbc
jh <dnwogus1212@gmail.com>
2024-08-19 03:14:01 +0900

    [우재현]  - Spring : Thymeleaf 반복문 Test 실습 완료

--- src/main/java/com/dnjh/spring/test/thymeleaf/Test02.java
+++ src/main/java/com/dnjh/spring/test/thymeleaf/Test02.java
@@ -1,7 +1,9 @@
 package com.dnjh.spring.test.thymeleaf;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Test02 {
-	@GetMapping("/thymeleaf/test02")
-	public String test02(Model model)
+	@GetMapping("/thymeleaf/test02/1")
+	public String music(Model model)
 	{
 		List<String> musicRanking = new ArrayList<>();
 		musicRanking.add("강남스타일");
@@ -20,6 +22,51 @@ public class Test02 {
 		musicRanking.add("보고싶다");
 		
 		model.addAttribute("musicRanking", musicRanking);
-		return "thymeleaf/test02";
+		return "thymeleaf/music";
+	}
+	
+	@GetMapping("/thymeleaf/test02/2")
+	public String member(Model model)
+	{
+		List<Map<String, Object>> membership = new ArrayList<>();
+
+		Map<String, Object> member = new HashMap<>();
+		member.put("name", "손오공");
+		member.put("phoneNumber", "010-1234-5678");
+		member.put("grade", "VIP");
+		member.put("point", 12040);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "저팔계");
+		member.put("phoneNumber", "010-4321-1234");
+		member.put("grade", "BASIC");
+		member.put("point", 3420);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "사오정");
+		member.put("phoneNumber", "010-8888-1111");
+		member.put("grade", "GOLD");
+		member.put("point", 1530);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "삼장");
+		member.put("phoneNumber", "010-0000-1234");
+		member.put("grade", "GOLD");
+		member.put("point", 8450);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "우마왕");
+		member.put("phoneNumber", "010-1111-2222");
+		member.put("grade", "BASIC");
+		member.put("point", 420);
+		membership.add(member);
+		
+		model.addAttribute("memberShip", membership);
+		
+		return "thymeleaf/member";
 	}
 }
