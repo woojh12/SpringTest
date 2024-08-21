414968ba1813b16187fa3af2a38e83a44b3dbc1d
jh <dnwogus1212@gmail.com>
2024-08-21 15:29:28 +0900

    [우재현]  - Spring : Thymeleaf 종합문제 Test

--- src/main/java/com/dnjh/spring/test/thymeleaf/Test05.java
+++ src/main/java/com/dnjh/spring/test/thymeleaf/Test05.java
@@ -1,14 +1,26 @@
 package com.dnjh.spring.test.thymeleaf;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dnjh.spring.test.database.domain.store;
+import com.dnjh.spring.test.database.service.StoreService;
 
 @Controller
 public class Test05 {
-	
+	@Autowired
+	private StoreService storeService;
 	@GetMapping("/thymeleaf/test05/info")
-	public String storeInfo()
+	public String storeInfo(Model model)
 	{
+		List<store> storeList = storeService.getStoreList();
+		
+		model.addAttribute("storeList", storeList);
+		
 		return "thymeleaf/test05-info";
 	}
 }
